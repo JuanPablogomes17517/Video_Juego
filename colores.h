2b2944365d94f20f33a192d5b75d67e6e9c4ba8e
Juan Pablo Gomes <45274200+JuanPablogomes17517@users.noreply.github.com>
2020-05-08 21:28:38 -0500

    Colores usados

---
+++ colores.h
@@ -0,0 +1,16 @@
+#ifndef _COLORES_H_
+#define _COLORES_H_
+
+#define BLACK   0x0000
+#define BLUE    0x001F
+#define RED     0xF800
+#define GREEN   0x07E0
+#define CYAN    0x07FF
+#define MAGENTA 0xF81F
+#define YELLOW  0xFFE0
+#define WHITE   0xFFFF
+#define OSCURO  0x0360
+#define VINO     0xE800
+#define NARANJA 0x99FF
+#define nar 0xA00A
+#endif
